bf2783ab09a7675fb5453a7d8b81de05f8919a1f
Damion Gans <16007964@student.hhs.nl>
2020-03-03 11:23:20 +0100

    work so far

--- Challenges/challenge02/install.sh
+++ Challenges/challenge02/install.sh
@@ -2,4 +2,8 @@
 ## Add the user challenge01 to system and give it a password
 sudo adduser challenge02 --disabled-login --gecos "" 
 echo challenge02:challenge02 | sudo chpasswd
-sudo mkdir -p /srv/.S€(R3])/app/.data
+
+serverfolderlocation = /srv/.S€(RE]/
+sudo mkdir $serverfolderlocation
+sudo cp -R ./app/ $serverfolderlocation
+sudo cp ./config $serverfolderlocation
