6e7175d10fba2ccaa49beb0ccd06de861e15facc
Damion Gans <16007964@student.hhs.nl>
2020-03-11 11:11:34 +0100

    Work

--- Challenges/challenge01/install.sh
+++ Challenges/challenge01/install.sh
@@ -1,10 +1,12 @@
-# Challenge 01 installation script
+#!/bin/bash
+# challenge01 installation script
 ## Add the user challenge01 to system and give it a password
 sudo adduser challenge01 --disabled-login --gecos "" 
 echo challenge01:challenge01 | sudo chpasswd
-## Copy the todo.txt file to the /home/challenge00/ and make user and group challenge00 the owner of it.  
-sudo cp ./todo.txt /home/challenge00/.
-sudo chown challenge00:challenge00 /home/challenge00/todo.txt
+## Copy the todo.txt file to the /home/challenge01/ and make user and group challenge01 the owner of it. Also set the right permissions
+sudo cp ./todo.txt /home/challenge01/.todo.txt
+sudo chown challenge00:challenge00 /home/challenge00/.todo.txt
+sudo chmod 775 /home/challenge01/.todo.txt
 ## Copy the .challenge01sshkeypassprahse file to /bin/ of challenge00 and make user root and group challenge00 the owner of it.  
 sudo cp ./.challenge01sshkeypassprahse /bin/.
 sudo chown root:challenge00 /bin/.challenge01sshkeypassprahse
--- Challenges/challenge02/install.sh
+++ Challenges/challenge02/install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-## Add the user challenge01 to system and give it a password
+# challenge02 installation script
+## Add the user challenge02 to system and give it a password
 sudo adduser challenge02 --disabled-login --gecos "" 
 echo challenge02:challenge02 | sudo chpasswd
+## Copy the todo.txt file to the /home/challenge02/ and make user and group challenge02 the owner of it. Also set the right permissions
+sudo cp ./todo.txt /home/challenge02/.todo.txt
+sudo chown challenge00:challenge00 /home/challenge00/.todo.txt
+sudo chmod 775 /home/challenge02/.todo.txt
 
-serverfolderlocation = /srv/.S€(RE]/
-sudo mkdir $serverfolderlocation
-sudo cp -R ./app/ $serverfolderlocation
-sudo cp ./config $serverfolderlocation
+sudo mkdir /srv/.S€CRE]/
+sudo cp -R ./app/ /srv/.S€CRE]/
+sudo cp ./config /srv/.S€CRE]/
+sudo cp ./startServer.sh /srv/.S€CRE]/
+sudo chmod 770 /srv/.S€CRE]/startServer.sh
+sudo chown challenge02:hackerman /srv/.S€CRE]/startServer.sh
+sudo mkdir /srv/hackerman/challenge02/
+sudo cp ./startServer-hackerman.sh /srv/hackerman/challenge02/
+sudo chmod 770 /srv/hackerman/challenge02/startServer-hackerman.sh
+sudo chown hackerman:root /srv/hackerman/challenge02/startServer-hackerman.sh
