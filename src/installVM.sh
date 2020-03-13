3d95dc5bfaf228f71a8582a80c6f01b5d1f4a136
Damion Gans <16007964@student.hhs.nl>
2020-03-13 17:24:14 +0100

    create challene02ftp user and place the passphrase there

--- src/installVM.sh
+++ src/installVM.sh
@@ -93,12 +93,13 @@ cp challenges/challenge02/index.html /srv/.S€CRE]/data/.
 echo "anon_root=/srv/.S€CRE]/data/" >> /etc/vsftpd.conf
 echo "listen_port=12345" >> /etc/vsftpd.conf
 echo "chroot_local_user=YES" >> /etc/vsftpd.conf
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 sed -i  s/anonymous_enable=NO/anonymous_enable=YES/g /etc/vsftpd.conf
 systemctl stop vsftpd
 systemctl disable vsftpd
 cp challenges/challenge02/passphrase /home/challenge02ftp/.
 chown challenge02ftp:challenge02ftp /home/challenge02ftp/passphrase
-chmod 774 /home/challenge02ftp/passphrase
+chmod 770 /home/challenge02ftp/passphrase
 cp challenges/challenge02/startServer.sh /srv/.S€CRE]/startServer.sh
 chmod 770 /srv/.S€CRE]/startServer.sh
 chown challenge02:hackerman /srv/.S€CRE]/startServer.sh
