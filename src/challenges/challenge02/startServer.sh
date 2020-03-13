135b21dd6dbd76e62799b39d0ecb9583bc4ef86d
Damion Gans <16007964@student.hhs.nl>
2020-03-13 11:31:51 +0100

    fixed challenge02

--- src/challenges/challenge02/startServer.sh
+++ src/challenges/challenge02/startServer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "02.startServer" >> /srv/hackerman/socket
 sleep 1.5
-while [[ "$(cat /srv/hackerman/.S€CRET/status)" != "02.startServer*" ]]; 
+while [[ "$(cat /srv/hackerman/status)" != "02.startServer*" ]]; 
 do 
     sleep 0.5; 
 done
-if [[ "$(cat /srv/hackerman/.S€CRET/status)" == "02.startServerSuccess" ]]
+if [[ "$(cat /srv/hackerman/status)" == "02.startServerSuccess" ]]
 then
     echo "Server succesfully started at port 12345. You can enter now..."
 else
