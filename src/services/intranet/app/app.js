5dc54dc1d927e021fbe885b484eeb7a93d78df06
Damion Gans <16007964@student.hhs.nl>
2020-03-17 12:17:09 +0100

    Removed old Checkkey function

--- src/services/intranet/app/app.js
+++ src/services/intranet/app/app.js
@@ -35,16 +35,6 @@ app.use(hackermanStateGetter);
 
 app.use('/', indexRouter);
 app.use('/hackerman', hackermanRouter);
-app.post('/checkKey', function (req, res) {
-    let secret = req.body.secret.toUpperCase();
-    if (secret === "7B4DBE9AC3E71126F3483890F17C9C9B94CF5BDD370F5FE16AF5D9B41D2C7784EE5CEED57E13127E6F7D3448131A8A64397210F9D2D6C3639482D4B06F4398D5") {
-        res.download('./SSH-Keys/challenge01', 'challenge01');
-    }
-    else {
-            res.end("Invalid secret send.");
-        }
-    
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res) {
