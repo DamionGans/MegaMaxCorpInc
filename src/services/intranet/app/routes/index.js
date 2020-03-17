b7098e23f7887b325f931b54c4a1c9d280ae001a
Damion Gans <16007964@student.hhs.nl>
2020-03-17 12:33:41 +0100

    fix ReferenceError: req is not defined in index.js

--- src/services/intranet/app/routes/index.js
+++ src/services/intranet/app/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let hackermanState = req.get('hackermanState');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    switch (hackermanState)
+    switch (req.get('hackermanState'))
     {
         case "00.installed":
             {
