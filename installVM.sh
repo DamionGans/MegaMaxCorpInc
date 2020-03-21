f1bf278980f4891ce33f9633397d0f59f3da69c1
Damion Gans <16007964@student.hhs.nl>
2020-03-21 15:48:06 +0000

    Do not download apt-get packages for they get downloaded into the current directory, which is not wantend

--- installVM.sh
+++ installVM.sh
@@ -7,8 +7,7 @@ apt-get -y dist-upgrade
 ## Add Node.js 12.x repositories
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 ## Install needed software for challenges with package manager APT
-apt-get download -y nodejs vsftpd
-apt-get install -y nodejs
+apt-get install -y nodejs vsftpd openssh-server
 ## Display greeting message when server boots to console
 echo MegaMaxCorpInc server booted. Please login to our intranet website at address \\4 |  tee /etc/issue
 ## Install the intranet service
@@ -32,7 +31,7 @@ echo "00.installed" > /srv/hackerman/state
 # install challenges (WIP as all challenges will eventually move to their own subfolder)
 # challenge00 installation script
 ## Add the user challenge00 to system and give it a password
-adduser challenge00 --disabled-login --gecos "" 
+adduser challenge00 --disabled-login --gecos ""
 echo challenge00:challenge00 |  chpasswd
 ## Copy the todo.txt file to the /home/challenge00/ and make user and group challenge00 the owner of it. Also set the right permissions
 cp challenges/challenge00/todo.txt /home/challenge00/todo.txt
@@ -49,7 +48,7 @@ chown challenge00:hackerman /home/challenge00/.supersecret
 chmod 770 /home/challenge00/.supersecret
 # challenge01 installation script
 ## Add the user challenge01 to system and give it a password
-adduser challenge01 --disabled-login --gecos "" 
+adduser challenge01 --disabled-login --gecos ""
 echo challenge01:challenge01 |  chpasswd
 ## Copy the todo.txt file to the /home/challenge01/ and make user and group challenge01 the owner of it. Also set the right permissions
 cp challenges/challenge01/todo.txt /home/challenge01/.todo.txt
@@ -60,14 +59,14 @@ mkdir /srv/hackerman/challenge01
 cp challenges/challenge01/handler.sh /srv/hackerman/challenge01/handler.sh
 chown hackerman:root /srv/hackerman/challenge01/handler.sh
 chmod 770 /srv/hackerman/challenge01/handler.sh
-## Copy the .challenge01sshkeypassprahse file to /bin/ of challenge01 and make user root and group challenge01 the owner of it.  
+## Copy the .challenge01sshkeypassprahse file to /bin/ of challenge01 and make user root and group challenge01 the owner of it.
 cp challenges/challenge01/challenge02sshkeypassprahse /bin/.challenge02sshkeypassprahse
 chown challenge01:hackerman /bin/.challenge02sshkeypassprahse
 ## Make /bin/.challenge01sshkeypassprahse executable
 chmod +x /bin/.challenge02sshkeypassprahse
 # challenge02 installation script
 ## Add the user challenge02 to system and give it a password
-adduser challenge02 --disabled-login --gecos "" 
+adduser challenge02 --disabled-login --gecos ""
 echo challenge02:challenge02 |  chpasswd
 ## Copy the todo.txt file to the /home/challenge02/ and make user and group challenge02 the owner of it. Also set the right permissions
 cp challenges/challenge02/todo.txt /home/challenge02/.todo.txt
@@ -79,7 +78,7 @@ cp challenges/challenge02/handler.sh /srv/hackerman/challenge02/handler.sh
 chown hackerman:root /srv/hackerman/challenge02/handler.sh
 chmod 770 /srv/hackerman/challenge02/handler.sh
 ## Create the Secret webserver config and startup files
-adduser challenge02ftp --disabled-login --gecos "" 
+adduser challenge02ftp --disabled-login --gecos ""
 echo challenge02ftp:password |  chpasswd
 mkdir /srv/.S€CRE]/
 cp challenges/challenge02/config /srv/.S€CRE]/.
