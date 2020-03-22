59b9c5a281ec8a8f22ed1e8fb8e0eca409e37c99
Damion Gans <16007964@student.hhs.nl>
2020-03-22 11:30:33 +0000

    Hackerman hacked to socket

--- services/intranet/app/hackermanWare.js
+++ services/intranet/app/hackermanWare.js
@@ -3,4 +3,8 @@ const getHackermanState = (req, res, next) => {
     req.app.set('hackermanState', fs.readFileSync('/srv/hackerman/state'));
     next();
 }
+const writeToHackermanSocket = (message) => {
+    fs.appendFileSync('/srv/hackerman/socket', message);
+}
 exports.getHackermanState = getHackermanState;
+exports.writeToHackermanSocket = writeToHackermanSocket;
--- services/intranet/app/routes/hackerman.js
+++ services/intranet/app/routes/hackerman.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var hackermanWare = require('../hackermanWare')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let hackermanState = req.app.get('hackermanState');
   if (hackermanState === null) {
-    // TODO: think about what to do.
+    hackermanWare.writeToHackermanSocket('00.hacked');
   }
-  if (hackermanState.includes('00.installed'))
-  {
-    
+  if (hackermanState.includes('00.installed')) {
+    hackermanWare.writeToHackermanSocket('00.hacked');
   }
   res.redirect('/hackerman.html');
 });
